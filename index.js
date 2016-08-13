5cc7dab73160eb8e6cc6afdd0b5f67d692ee5592
Michael Kane <mk+github@mk4p.com>
2016-08-13 19:31:24 -0400

    WIP

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
 'use strict';
+require('dotenv').load({silent: true});
 
 const _ = require('lodash');
-const hue = require('node-hue-api');
+const log = console.log;
 const Promise = require('bluebird');
 
-const log = console.log;
-const displayResult = log;
-const exit = function(){process.exit(0)}
+require('./lib/api').then(function(result){
 
-const default_id = "08a902b95915cdd9b75547cb50892dc4";
-const huebot_id = process.env.HUEBOT_ID;
+    const hue = result.hue;
+    const api = result.api;
 
-hue.nupnpSearch()
-    .then(function(bridges){
-        if(!bridges.length){
-            log('no bridges found');
-            exit();
-        }
+    //Set bar light to random brightness
+    let bright = parseInt(Math.random() * 100, 10);
+    let state = hue.lightState.create().on().white(500, 100);
 
-        let bridge = bridges[0];
-        log(bridge);
-        let api = new hue.HueApi(bridge.ipaddress, huebot_id);
+    log('setting bright', bright);
 
-        api.config().then(function(config){
-
-            log('got config:', config);
-
-            api.getFullState().then(displayResult).done();
+    api.setLightState(2, state, function(err, result){
+        console.error(err);
+        log(result);
+        process.exit(0);
+    });
 
 
-        }).done();
 
+});
 
-}).done();
+/*
+var hue = require("node-hue-api"),
+    HueApi = hue.HueApi,
+    lightState = hue.lightState;
 
+var displayResult = function(result) {
+    console.log(JSON.stringify(result, null, 2));
+};
 
+var host = "192.168.1.140",
+    username = process.env.HUEBOT_ID,
+    api = new HueApi(host, username),
+    state;
+
+// Set light state to 'on' with warm white value of 500 and brightness set to 100%
+let bright = parseInt(Math.random() * 100, 10);
+state = lightState.create().on().white(500, bright);
+
+// // --------------------------
+// // Using a promise
+// api.setLightState(2, state)
+//     .then(displayResult)
+//     .done();
+
+// --------------------------
+// Using a callback
+api.setLightState(2, state, function(err, lights) {
+    if (err) throw err;
+    displayResult(lights);
+});
+*/
